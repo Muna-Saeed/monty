582fb81d6cccb45ed4be579ec3a3066e41e665f8
Muna-Saeed <munasaeed6367@gmail.com>
2023-10-17 16:35:06 -0700

    updated

--- push-pall.c
+++ push-pall.c
@@ -1,7 +1,5 @@
 #include "monty.h"
 
-stack_t *stack = NULL;
-
 void push(stack_t **stack, unsigned int line_number)
 {
 	if (!stack)
@@ -11,6 +9,7 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 
 	stack_t *new_node = malloc(sizeof(stack_t));
+
 	if (!new_node)
 	{
 		fprintf(stderr, "Error: Failed to allocate memory\n");
@@ -32,9 +31,60 @@ void pall(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 
 	stack_t *current = *stack;
+
 	while (current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 	}
+}
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *temp = *stack;
+	*stack = (*stack)->next;
+
+	if (*stack)
+		(*stack)->prev = NULL;
+
+	free(temp);
+}
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *top = *stack;
+	stack_t *second = top->next;
+
+	top->next = second->next;
+	if (second->next)
+		second->next->prev = top;
+
+	second->prev = NULL;
+	second->next = top;
+	top->prev = second;
+
+	*stack = second;
 }
