94018c99738a870e7701535b25e6ce3dd03e95c9
Muna-Saeed <munasaeed6367@gmail.com>
2023-10-18 12:25:08 -0700

    updated

--- main.c
+++ main.c
@@ -1,41 +1,44 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include "monty.h"
 
 int main(int argc, char *argv[])
 {
+	FILE *file;
+	stack_t *stack = NULL;
+	char *line = NULL;
+	size_t line_len = 0;
+	ssize_t read;
+	unsigned int line_number;
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	FILE *file = fopen(argv[1], "r");
+	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
-	stack_t *stack = NULL;
-	char *line = NULL;
-	size_t line_len = 0;
-	ssize_t read;
+	/* Update line number accordingly */
+	line_number= 1;
 
 	while ((read = getline(&line, &line_len, file)) != -1)
 	{
-		unsigned int line_number = 1; // Update line number accordingly
-
 		char *opcode = strtok(line, " \t\n");
 		if (opcode != NULL)
 		{
 			instruction_t instruction;
-
 			instruction.opcode = opcode;
-			instruction.f = NULL; // Set the function pointer to NULL initially
+			instruction.f = NULL;
 
-			// Determine the appropriate function pointer based on opcode
 			if (strcmp(opcode, "push") == 0)
 				instruction.f = push;
 			else if (strcmp(opcode, "pall") == 0)
@@ -52,13 +55,13 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		line_number++; // Increment line number for each line read
+		line_number++;
 	}
 
 	fclose(file);
 	free(line);
 
-	// Clean up the stack if needed
+	/* Clean up the stack if needed */
 
 	return EXIT_SUCCESS;
 }
