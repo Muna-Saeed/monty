4135ada4d4055c3c944bda6d2c1cc8c50223aede
Muna-Saeed <munasaeed6367@gmail.com>
2023-10-18 12:09:56 -0700

    updated

--- main.c
+++ main.c
@@ -1,107 +1,64 @@
-#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-stack_t *stack = NULL;
-
-void push(stack_t **stack, unsigned int line_number)
-{
-	if (!stack)
-	{
-		fprintf(stderr, "Error: Stack is NULL\n");
-		exit(EXIT_FAILURE);
-	}
-
-	stack_t *new_node = malloc(sizeof(stack_t));
-	if (!new_node)
-	{
-		fprintf(stderr, "Error: Failed to allocate memory\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = line_number;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack)
-		(*stack)->prev = new_node;
-
-	*stack = new_node;
-}
-
-void pall(stack_t **stack, unsigned int line_number)
-{
-	(void)line_number;
-
-	stack_t *current = *stack;
-	while (current)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
-}
-
-instruction_t get_instruction(char *opcode)
-{
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		/* Add more opcodes and corresponding functions here */
-		{NULL, NULL}
-	};
-
-	int i = 0;
-	while (instructions[i].opcode != NULL)
-	{
-		if (strcmp(instructions[i].opcode, opcode) == 0)
-			return instructions[i];
-		i++;
-	}
-
-	return instructions[i];  // Return a default instruction if opcode not found
-}
+#include "monty.h"
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return EXIT_FAILURE;
+		fprintf(stderr, "Usage: monty file\n");
+		exit(EXIT_FAILURE);
 	}
 
 	FILE *file = fopen(argv[1], "r");
-	if (!file)
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
-	char line[100];
-	unsigned int line_number = 0;
-
 	stack_t *stack = NULL;
-	instruction_t instruction;
+	char *line = NULL;
+	size_t line_len = 0;
+	ssize_t read;
 
-	while (fgets(line, sizeof(line), file) != NULL)
+	while ((read = getline(&line, &line_len, file)) != -1)
 	{
-		line_number++;
+		unsigned int line_number = 1; // Update line number accordingly
 
-		// Tokenize the line
 		char *opcode = strtok(line, " \t\n");
-		if (!opcode)
-			continue;  // Empty line, skip to the next iteration
-
-		// Get the corresponding instruction
-		instruction = get_instruction(opcode);
-		if (!instruction.f)
+		if (opcode != NULL)
 		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-			fclose(file);
-			return EXIT_FAILURE;
+			instruction_t instruction;
+
+			instruction.opcode = opcode;
+			instruction.f = NULL; // Set the function pointer to NULL initially
+
+			// Determine the appropriate function pointer based on opcode
+			if (strcmp(opcode, "push") == 0)
+				instruction.f = push;
+			else if (strcmp(opcode, "pall") == 0)
+				instruction.f = pall;
+
+			if (instruction.f != NULL)
+				instruction.f(&stack, line_number);
+			else
+			{
+				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+				fclose(file);
+				free(line);
+				exit(EXIT_FAILURE);
+			}
 		}
 
-		// Execute the instruction's function
-		instruction.f(&stack, line_number);
+		line_number++; // Increment line number for each line read
 	}
 
 	fclose(file);
+	free(line);
+
+	// Clean up the stack if needed
+
 	return EXIT_SUCCESS;
 }
