718b05fae9e8d3f3dc50ede7d1bec1c733dd41d6
abdiwoli <abdiwolih@gmail.com>
2023-10-20 00:24:35 +0300

    update

--- advanced.c
+++ advanced.c
@@ -66,8 +66,8 @@ void pstr(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pstr, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
+		write(STDOUT_FILENO, "\n", 1);
+		return;
 	}
 	(void)line_number;
 	while(current != NULL)
